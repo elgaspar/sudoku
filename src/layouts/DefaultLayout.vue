2f554ba2a2ae3cd6bdcdd950307f3feff24f86c7
Elias Gasparis <iliasgas@gmail.com>
2025-03-22 11:13:27 +0200

    layout improvement

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -4,7 +4,7 @@ import Logo from '@/components/Logo.vue';
 </script>
 
 <template>
-    <div class="min-h-full">
+    <div class="flex flex-col min-h-screen">
         <header>
             <div class="navbar bg-neutral text-neutral-content gap-3">
                 <Logo />
@@ -12,7 +12,7 @@ import Logo from '@/components/Logo.vue';
             </div>
         </header>
 
-        <main class="bg-base-100 py-5">
+        <main class="bg-base-100 py-5 flex-1">
             <div class="mx-auto max-w-7xl">
                 <div class="rounded-lg bg-white p-6 shadow-sm">
                     <slot></slot>
